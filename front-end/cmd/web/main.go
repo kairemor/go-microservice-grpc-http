f60e0ed3fcfb9e712fa8ccc58eb029287567e6fa
Mor Kaire <kairemor@gmail.com>
2022-10-28 11:47:21 +0000

    repo init & frontend & broker service & dockerfile & makefile

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var port string
+	flag.StringVar(&port, "port", "4000", "port to listen")
+	flag.Parse()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port ", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
