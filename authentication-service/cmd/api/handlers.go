3d3d9dbb09efa22a102f0de8bbc0ad09b1e70d76
Mor Kaire <kairemor@gmail.com>
2022-10-30 16:20:40 +0000

    add and conf logger service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -17,30 +17,30 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		Password string `json:"password"`
 	}
 
-	err := tools.readJSON(w, r, &requestPayload)
+	err := tools.ReadJSON(w, r, &requestPayload)
 	if err != nil {
-		tools.errorJSON(w, err, http.StatusBadRequest)
+		tools.ErrorJSON(w, err, http.StatusBadRequest)
 		return
 	}
 
 	// validate the user against the database
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		tools.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		tools.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	payload := tools.jsonResponse {
+	payload := toolbox.JSONResponse {
 		Error: false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
 		Data: user,
 	}
 
-	tools.writeJSON(w, http.StatusAccepted, payload)
+	tools.WriteJSON(w, http.StatusAccepted, payload)
 }
